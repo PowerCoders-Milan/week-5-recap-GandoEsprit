f549afcb58ebe78e9ba308125236a7b9ca805574
gando diallo <gandoesprit@gmail.com>
2021-03-05 15:34:08 +0100

    Solution to exercice tree in js

--- js-ex3/exercise.js
+++ js-ex3/exercise.js
@@ -1,12 +1,13 @@
-formula(1, 3, 5) //=> 8
-formula(3, 4, 6) //=> 18
-formula(2, 4) //=> 9
-formula(5, 6) //=> 31
-formula(3) //=> 4
-formula(5) //=> 6
-formula() //=> 2
+console.log(formula(1, 3, 5)) //=> 8
+console.log(formula(3, 4, 6)) //=> 18
+console.log(formula(2, 4)) //=> 9
+console.log(formula(5, 6)) //=> 31
+console.log(formula(3)) //=> 4
+console.log(formula(5)) //=> 6
+console.log(formula()) //=> 2
 
 // review all the possible parameters
-function formula(){
-
+function formula(num1 = 1, num2 = 1, num3 = 1){
+    return num2 * num1 + num3;
 }
+
